b2f2bad41f2a6af68382d5a690045a2e527cc53c
nmlgc <nmlgc@nmlgc.net>
2017-07-05 19:58:00 +0200

    Add #pragma comment(lib) statements for the remaining default libraries.
    Visual Studio adds these by default, but cl.exe doesn't.

--- win32_utf8_build_dynamic.c
+++ win32_utf8_build_dynamic.c
@@ -20,8 +20,12 @@
 
 #pragma comment(lib, "comdlg32.lib")
 #pragma comment(lib, "dsound.lib")
+#pragma comment(lib, "gdi32.lib")
+#pragma comment(lib, "ole32.lib")
 #pragma comment(lib, "psapi.lib")
+#pragma comment(lib, "shell32.lib")
 #pragma comment(lib, "shlwapi.lib")
+#pragma comment(lib, "user32.lib")
 #pragma comment(lib, "version.lib")
 #pragma comment(lib, "wininet.lib")
 
