376a1b426d049c41f7d23db00b086b39d515bacd
32th System <intensonat@gmail.com>
2019-07-15 20:13:53 +0200

    Add functions mentioned by Issue #3

--- src/shlwapi_dll.c
+++ src/shlwapi_dll.c
@@ -11,6 +11,12 @@ const w32u8_pair_t shlwapi_pairs[] = {
 	{"PathFindFileNameA", PathFindFileNameU},
 	{"PathMatchSpecA", PathMatchSpecU},
 	{"PathRemoveFileSpecA", PathRemoveFileSpecU},
+	{"PathAddBackslashA", PathAddBackslashU},
+	{"PathIsDirectoryA", PathIsDirectoryU},
+	{"PathIsRelativeA", PathIsRelativeU},
+	{"PathCanonicalizeA", PathCanonicalizeU},
+	{"PathCombineA", PathCombineU},
+	{"PathAppendA", PathAppendU},
 	{ NULL }
 };
 
@@ -68,4 +74,95 @@ BOOL STDAPICALLTYPE PathRemoveFileSpecU(
 		return 1;
 	}
 	return 0;
+}
+
+LPSTR STDAPICALLTYPE PathAddBackslashU(
+	LPSTR pszPath
+)
+{
+	if (pszPath == 0 || *pszPath == '\x00') {
+		return NULL;
+	}
+	size_t pszPathLen = strlen(pszPath);
+	if (pszPath[pszPathLen - 1]  != '\\') {
+		pszPath[pszPathLen] = '\\';
+		pszPath[pszPathLen + 1] = '\x00';
+		return (pszPath + pszPathLen + 1);
+	} else {
+		return (pszPath + pszPathLen);
+	}
+}
+
+BOOL STDAPICALLTYPE PathIsDirectoryU(
+	LPCSTR pszPath
+)
+{
+	return Wrap1P((Wrap1PFunc_t*)PathIsDirectoryW, pszPath);
+}
+
+BOOL STDAPICALLTYPE PathIsRelativeU(
+	LPCSTR pszPath
+)
+{
+	return (*pszPath != '\\' && *pszPath != '/') && (*(pszPath + 1) != ':');
+}
+
+BOOL STDAPICALLTYPE PathCanonicalizeU(
+	LPSTR pszBuf,
+	LPCSTR pszPath
+)
+{	// This function may get reimplemented https://doxygen.reactos.org/de/dff/dll_2win32_2shlwapi_2path_8c.html#aa31be5d2410fbd8564ec0da929354a0f 
+	wchar_t pszBuf_w[MAX_PATH];
+	WCHAR_T_DEC(pszPath);
+	WCHAR_T_CONV(pszPath);
+	BOOL ret = PathCanonicalizeW(pszBuf_w, pszPath_w);
+
+	WCHAR_T_FREE(pszPath);
+
+	if (!ret) {
+		return FALSE;
+	}
+
+	SetLastError(0);
+	StringToUTF8(pszBuf, pszBuf_w, MAX_PATH);
+	if (GetLastError() == ERROR_INSUFFICIENT_BUFFER) {
+		return FALSE;
+	}
+
+	return TRUE;
+}
+
+LPSTR STDAPICALLTYPE PathCombineU(
+	LPSTR pszDest,
+	LPCSTR pszDir,
+	LPCSTR pszFile
+)
+{
+	strcpy(pszDest, pszDir);
+	if (PathIsRelativeU(pszFile)) {
+		
+		LPSTR AppendPointer = PathAddBackslashU(pszDir);
+		strcpy(AppendPointer, pszFile);
+	}
+	else {
+		strcpy(pszDest, pszFile);
+	}
+	char final_buffer[MAX_PATH];
+	BOOL ret = PathCanonicalizeU(final_buffer, pszDest);
+	if (!ret) {
+		return NULL;
+	}
+	strcpy(pszDest, final_buffer);
+	return pszDest;
+}
+
+BOOL STDAPICALLTYPE PathAppendU(
+	LPSTR pszPath,
+	LPCSTR pszMore
+)
+{
+	if (!PathCombineU(pszPath, pszPath, pszMore)) {
+		return FALSE;
+	}
+	return TRUE;
 }
--- src/shlwapi_dll.h
+++ src/shlwapi_dll.h
@@ -32,3 +32,43 @@ WRAPPER_DEC(BOOL STDAPICALLTYPE, PathRemoveFileSpec,
 );
 #undef PathRemoveFileSpec
 #define PathRemoveFileSpec PathRemoveFileSpecU
+
+WRAPPER_DEC(LPSTR STDAPICALLTYPE, PathAddBackslash,
+	LPSTR pszPath
+);
+#undef PathAddBackslash
+#define PathAddBackslash PathAddBackslashU
+
+WRAPPER_DEC(BOOL STDAPICALLTYPE, PathIsDirectory,
+	LPCSTR pszPath
+);
+#undef PathIsDirectory
+#define PathIsDirectory PathIsDirectoryU
+
+WRAPPER_DEC(BOOL STDAPICALLTYPE, PathIsRelative,
+	LPCSTR pszPath
+);
+#undef PathIsRelative
+#define PathIsRelative PathIsRelativeU
+
+WRAPPER_DEC(BOOL STDAPICALLTYPE, PathCanonicalize,
+	LPSTR pszBuf,
+	LPCSTR pszPath
+);
+#undef PathCanonicalize
+#define PathCanonicalize PathCanonicalizeU
+
+WRAPPER_DEC(LPSTR STDAPICALLTYPE, PathCombine,
+	LPSTR pszDest,
+	LPCSTR pszDir,
+	LPCSTR pszFile
+);
+#undef PathCombine
+#define PathCombine PathCombineU
+
+WRAPPER_DEC(BOOL STDAPICALLTYPE, PathAppend,
+	LPSTR pszPath,
+	LPCSTR pszMore
+);
+#undef PathAppend
+#define PathAppend PathAppendU
